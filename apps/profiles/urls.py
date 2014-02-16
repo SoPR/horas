2ec0fa93300141ea66105dd509042557f7673ab5
José Padilla <jpadilla@webapplicate.com>
2014-02-16 12:30:05 -0400

    Implement placeholders for profile endpoints

--- apps/profiles/urls.py
+++ apps/profiles/urls.py
@@ -1,9 +1,11 @@
 from django.conf.urls import patterns, url
-from .views import ProfileDetailView
+from .views import ProfileDetailView, ProfileUpdateView
 
 urlpatterns = patterns(
     '',  # Empty string as prefix
 
-    url('^(?P<username>.*)/$', ProfileDetailView.as_view(), name='profile_detail'),
-    # url('^$', ProfileDetailView.as_view(), name='profile_detail')
+    url('^/$', ProfileDetailView.as_view(), name='profile_detail'),
+    url('^/update/$', ProfileUpdateView.as_view(), name='profile_update'),
+
+    # url('^.(?P<format>ics|rss|atom)$', ProfileDetailView.as_view()),
 )
--- horas/urls.py
+++ horas/urls.py
@@ -7,7 +7,7 @@ urlpatterns = patterns(
     '',  # Empty string as prefix
 
     url(r'^admin/', include(admin.site.urls)),
-    url(r'^profiles/', include('apps.profiles.urls')),
+    url(r'^(?P<username>[-\w]+)', include('apps.profiles.urls')),
 
     url(r'^accounts/', include('allauth.urls')),
 
