03ea0760f3ee4a80f2bd6ea265caffe7842deafe
José Padilla <jpadilla@webapplicate.com>
2014-02-23 13:14:24 -0400

    Implement management command to create meetings

---
+++ apps/profiles/management/commands/create_meetings.py
@@ -0,0 +1,22 @@
+from django.core.management.base import BaseCommand
+
+from apps.profiles.models import User
+
+
+class Command(BaseCommand):
+    args = '<user_id user_id ...>'
+    help = 'Creates meeting slots for the specified users.'
+
+    def handle(self, *args, **options):
+        meetings_created = []
+
+        if args:
+            users = User.objects.all(pk__in=args)
+        else:
+            users = User.objects.all()
+
+        for user in users:
+            meetings_created.append(user.create_meeting())
+
+        self.stdout.write('Succesfully created {} meetings.'.format(
+            len(meetings_created)))
