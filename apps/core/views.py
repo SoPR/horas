8ec037266f6956e982cf30a6042dc82460cb6c71
José Padilla <jpadilla@webapplicate.com>
2014-02-24 10:33:24 -0400

    Plug in users count in home page

--- apps/core/views.py
+++ apps/core/views.py
@@ -10,7 +10,8 @@ class HomePageView(TemplateView):
         context = super(HomePageView, self).get_context_data()
 
         context.update({
-            'featured_users': User.objects.filter(featured=True)
+            'featured_users': User.objects.filter(featured=True),
+            'users_count': User.objects.count()
         })
 
         return context
