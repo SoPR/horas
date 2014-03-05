e24864bc991612a485eeca5d1e79be5f57b6fcae
Giovanni Collazo <hello@gcollazo.com>
2014-03-04 22:03:05 -0400

    Adds active_link template tag and hooks almost all of the header

---
+++ apps/core/templatetags/active_link.py
@@ -0,0 +1,18 @@
+import re
+from django import template
+
+register = template.Library()
+
+
+@register.simple_tag
+def active_link(request, pattern):
+    '''
+    Simple template tag that will return the string active if the passed
+    pattern is found in the current url. We user this to highlight the
+    current menu item.
+    '''
+    path = '/{}/'.format(request.path.split('/')[1])
+
+    if re.search(pattern, path):
+        return 'active'
+    return ''
