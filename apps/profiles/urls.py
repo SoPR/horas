8d1fa06fa83e99ede2dc49b4b02749b0714cef78
Giovanni Collazo <hello@gcollazo.com>
2014-03-04 22:02:17 -0400

    Fix profile_detail url

--- apps/profiles/urls.py
+++ apps/profiles/urls.py
@@ -4,7 +4,7 @@ from .views import ProfileDetailView, ProfileUpdateView
 urlpatterns = patterns(
     '',  # Empty string as prefix
 
-    url('^/$', ProfileDetailView.as_view(), name='profile_detail'),
+    url('^$', ProfileDetailView.as_view(), name='profile_detail'),
     url('^/update/$', ProfileUpdateView.as_view(), name='profile_update'),
 
     # url('^.(?P<format>ics|rss|atom)$', ProfileDetailView.as_view()),
