3eb3a3df9dd137e9ce0d445f18145fb37ae33672
Giovanni Collazo <hello@gcollazo.com>
2014-03-04 22:56:35 -0400

    Basic profile page setup

--- apps/search/urls.py
+++ apps/search/urls.py
@@ -5,4 +5,5 @@ urlpatterns = patterns(
     '',  # Empty string as prefix
 
     url('^$', SearchView.as_view(), name='search_list'),
+    url('^\?q=(?P<query>[-\w]+)', SearchView.as_view(), name='search_query'),
 )
