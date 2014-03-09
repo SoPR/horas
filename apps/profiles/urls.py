6c3dc13637b20b664350d1c228d6d9f549801b13
Giovanni Collazo <hello@gcollazo.com>
2014-03-09 13:54:57 -0400

    Add confi and logout links, basic setup for profile update page

--- apps/profiles/urls.py
+++ apps/profiles/urls.py
@@ -5,7 +5,7 @@ urlpatterns = patterns(
     '',  # Empty string as prefix
 
     url('^$', ProfileDetailView.as_view(), name='profile_detail'),
-    url('^/update/$', ProfileUpdateView.as_view(), name='profile_update'),
+    url('update/$', ProfileUpdateView.as_view(), name='profile_update'),
 
     # url('^.(?P<format>ics|rss|atom)$', ProfileDetailView.as_view()),
 )
