76829380376c31ea3f1e899770d1edffd1afc047
Giovanni Collazo <hello@gcollazo.com>
2014-03-15 14:20:46 -0400

    Change gravatar url to use https

--- apps/profiles/utils.py
+++ apps/profiles/utils.py
@@ -3,4 +3,4 @@ import hashlib
 
 def get_gravatar_url(email):
     email_hash = hashlib.md5(email.lower().encode('utf-8')).hexdigest()
-    return "http://www.gravatar.com/avatar/{}".format(email_hash)
+    return "https://www.gravatar.com/avatar/{}".format(email_hash)
