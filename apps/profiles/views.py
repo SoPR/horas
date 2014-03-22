729a814f976ceffba0092ba56efca8ccea06e6e6
Giovanni Collazo <hello@gcollazo.com>
2014-03-22 16:36:29 -0400

    Add message on profile form save

--- apps/profiles/views.py
+++ apps/profiles/views.py
@@ -1,5 +1,7 @@
 from django.views.generic import DetailView, UpdateView
 from django.core.urlresolvers import reverse_lazy
+from django.contrib import messages
+from django.utils.translation import ugettext_lazy as _
 
 from braces.views import LoginRequiredMixin
 
@@ -28,3 +30,7 @@ class ProfileUpdateView(LoginRequiredMixin, UpdateView):
 
     def get_success_url(self):
         return reverse_lazy('profile_update', args=[self.object.username])
+
+    def form_valid(self, *args, **kwargs):
+        messages.success(self.request, _('Perfil guardado con exito'))
+        return super(ProfileUpdateView, self).form_valid(*args, **kwargs)
