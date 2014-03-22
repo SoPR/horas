5d0ac87a4a23cb328fe03186690b98f86f1c199b
Giovanni Collazo <hello@gcollazo.com>
2014-03-22 19:55:52 -0400

    Moves to utils.next_weekday to apps.profiles

--- apps/meetings/utils.py
+++
@@ -1,7 +0,0 @@
-import datetime
-
-def next_weekday(date, weekday):
-    days_ahead = weekday - date.weekday()
-    if days_ahead <= 0: # Target day already happened this week
-        days_ahead += 7
-    return date + datetime.timedelta(days_ahead)
--- apps/profiles/utils.py
+++ apps/profiles/utils.py
@@ -1,6 +1,14 @@
 import hashlib
+import datetime
 
 
 def get_gravatar_url(email):
     email_hash = hashlib.md5(email.lower().encode('utf-8')).hexdigest()
     return "https://www.gravatar.com/avatar/{}".format(email_hash)
+
+
+def next_weekday(date, weekday):
+    days_ahead = weekday - date.weekday()
+    if days_ahead <= 0: # Target day already happened this week
+        days_ahead += 7
+    return date + datetime.timedelta(days_ahead)
