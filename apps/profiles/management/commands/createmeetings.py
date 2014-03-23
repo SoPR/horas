56a138a6cde8a963ddf86a5f3555e9f16f58cf1a
Giovanni Collazo <hello@gcollazo.com>
2014-03-22 23:01:39 -0400

    Replaced create meetings command

--- apps/profiles/management/commands/createmeetings.py
+++ apps/profiles/management/commands/createmeetings.py
@@ -15,24 +15,31 @@ from django.core.management.base import BaseCommand
 from ....profiles.models import User
 
 class Command(BaseCommand):
+    args = '<user_id user_id ...>'
+    help = 'Creates meeting slots for the specified users.'
 
     def handle(self, *args, **options):
         run_start = time.time()
-        meetings_created = 0
-        users = User.objects.filter(is_active=True)
+        meetings_created = []
+
+        if args:
+            users = User.objects.filter(pk__in=args, is_active=True)
+        else:
+            users = User.objects.filter(is_active=True)
+
 
         for user in users:
             m, created = user.create_meeting_slot()
 
             if created:
-                meetings_created += 1
-                self.stdout.write(
-                    '--> Created meeting for user {0} at {1}'.format(m.mentor, m.datetime))
-
+                meetings_created.append(m)
+                meesage = '--> Created meeting for user {0} on {1}'
+                self.stdout.write(meesage.format(m.mentor, m.datetime))
 
         run_time = round(time.time() - run_start)
+
         if meetings_created > 0:
-            self.stdout.write(
-                '--> Created meetings for {0} users in {1} secs'.format(meetings_created, run_time))
+            message = '--> Succesfully created {0} meetings in {1} secs'
+            self.stdout.write(message.format(len(meetings_created), run_time))
         else:
             self.stdout.write('--> No meetings creted, run time {0} secs'.format(run_time))
