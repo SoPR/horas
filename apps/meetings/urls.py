a597db2a56f356d9620bad8d8a8ff8305ea82e91
Giovanni Collazo <hello@gcollazo.com>
2014-03-29 22:18:03 -0400

    fix confirm appereance

--- apps/meetings/urls.py
+++ apps/meetings/urls.py
@@ -5,5 +5,6 @@ urlpatterns = patterns(
     '',  # Empty string as prefix
 
     url('^(?P<pk>\d+)/$', MeetingUpdateView.as_view(), name='meeting_detail'),
+    url('^(?P<pk>\d+)/request/$', MeetingUpdateView.as_view(), name='meeting_update'),
     url('^(?P<pk>\d+)/confirm/$', MeetingConfirmView.as_view(), name='meeting_confirm'),
 )
