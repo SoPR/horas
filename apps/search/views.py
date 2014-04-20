206433d01ef8b3b4327fc2254fbe02d7d91b3f0b
Giovanni Collazo <hello@gcollazo.com>
2014-04-20 18:23:53 -0400

    Adds pagination to search

--- apps/search/views.py
+++ apps/search/views.py
@@ -12,6 +12,7 @@ from ..profiles.models import User
 class SearchView(ListView):
     queryset = User.objects.all().order_by('-date_joined')
     template_name = 'search/search.html'
+    paginate_by = 9
 
     def get_context_data(self):
         context = super(SearchView, self).get_context_data()
