5c94592cb6bf8c861e3f85ff47649ac1f3d2a9d5
Giovanni Collazo <hello@gcollazo.com>
2014-04-20 18:51:59 -0400

    Redirect logged in users to search page

--- apps/core/views.py
+++ apps/core/views.py
@@ -1,10 +1,18 @@
 from django.views.generic.base import TemplateView
+from django.http import HttpResponseRedirect
+from django.core.urlresolvers import reverse_lazy
 
 from ..profiles.models import User
 
 
 class HomePageView(TemplateView):
     template_name = 'home.html'
+
+    def dispatch(self, *args, **kwargs):
+        if self.request.user.is_authenticated():
+            return HttpResponseRedirect(reverse_lazy('search_list'))
+
+        return super(HomePageView, self).dispatch(*args, **kwargs)
 
     def get_context_data(self):
         context = super(HomePageView, self).get_context_data()
