8858c1270150b7eeefa39645a261f7d44cd309fe
Giovanni Collazo <hello@gcollazo.com>
2014-05-06 17:08:38 -0400

    Merge all migrations

--- apps/profiles/migrations/0001_initial.py
+++ apps/profiles/migrations/0001_initial.py
@@ -9,8 +9,8 @@ class Migration(SchemaMigration):
 
     def forwards(self, orm):
         # Adding model 'User'
-        db.create_table('profiles_user', (
-            ('id', self.gf('django.db.models.fields.AutoField')(primary_key=True)),
+        db.create_table(u'profiles_user', (
+            (u'id', self.gf('django.db.models.fields.AutoField')(primary_key=True)),
             ('password', self.gf('django.db.models.fields.CharField')(max_length=128)),
             ('last_login', self.gf('django.db.models.fields.DateTimeField')(default=datetime.datetime.now)),
             ('is_superuser', self.gf('django.db.models.fields.BooleanField')(default=False)),
@@ -21,13 +21,19 @@ class Migration(SchemaMigration):
             ('is_staff', self.gf('django.db.models.fields.BooleanField')(default=False)),
             ('is_active', self.gf('django.db.models.fields.BooleanField')(default=True)),
             ('date_joined', self.gf('django.db.models.fields.DateTimeField')(default=datetime.datetime.now)),
+            ('featured', self.gf('django.db.models.fields.BooleanField')(default=False)),
             ('bio', self.gf('django.db.models.fields.TextField')(blank=True)),
             ('twitter_username', self.gf('django.db.models.fields.CharField')(max_length=50, blank=True)),
             ('facebook_username', self.gf('django.db.models.fields.CharField')(max_length=50, blank=True)),
             ('github_username', self.gf('django.db.models.fields.CharField')(max_length=50, blank=True)),
             ('website_url', self.gf('django.db.models.fields.URLField')(max_length=50, blank=True)),
-            ('day_of_week', self.gf('django.db.models.fields.CharField')(max_length=1, db_index=True, blank=True)),
+            ('gravatar_url', self.gf('django.db.models.fields.URLField')(max_length=200, blank=True)),
+            ('is_gravatar_verified', self.gf('django.db.models.fields.BooleanField')(default=False)),
+            ('city', self.gf('django.db.models.fields.CharField')(max_length=50, blank=True)),
+            ('state', self.gf('django.db.models.fields.CharField')(max_length=50, blank=True)),
+            ('day_of_week', self.gf('django.db.models.fields.IntegerField')(db_index=True, max_length=1, null=True, blank=True)),
             ('start_time', self.gf('django.db.models.fields.TimeField')(null=True, blank=True)),
+            ('timezone', self.gf('django.db.models.fields.CharField')(default='America/Puerto_Rico', max_length=255)),
             ('phone', self.gf('django.db.models.fields.CharField')(max_length=50, blank=True)),
             ('skype', self.gf('django.db.models.fields.CharField')(max_length=50, blank=True)),
             ('google', self.gf('django.db.models.fields.CharField')(max_length=50, blank=True)),
@@ -35,74 +41,78 @@ class Migration(SchemaMigration):
             ('address', self.gf('django.db.models.fields.TextField')(blank=True)),
             ('date_updated', self.gf('django.db.models.fields.DateTimeField')(auto_now=True, blank=True)),
         ))
-        db.send_create_signal('profiles', ['User'])
+        db.send_create_signal(u'profiles', ['User'])
 
         # Adding M2M table for field groups on 'User'
-        m2m_table_name = db.shorten_name('profiles_user_groups')
+        m2m_table_name = db.shorten_name(u'profiles_user_groups')
         db.create_table(m2m_table_name, (
             ('id', models.AutoField(verbose_name='ID', primary_key=True, auto_created=True)),
-            ('user', models.ForeignKey(orm['profiles.user'], null=False)),
-            ('group', models.ForeignKey(orm['auth.group'], null=False))
+            ('user', models.ForeignKey(orm[u'profiles.user'], null=False)),
+            ('group', models.ForeignKey(orm[u'auth.group'], null=False))
         ))
         db.create_unique(m2m_table_name, ['user_id', 'group_id'])
 
         # Adding M2M table for field user_permissions on 'User'
-        m2m_table_name = db.shorten_name('profiles_user_user_permissions')
+        m2m_table_name = db.shorten_name(u'profiles_user_user_permissions')
         db.create_table(m2m_table_name, (
             ('id', models.AutoField(verbose_name='ID', primary_key=True, auto_created=True)),
-            ('user', models.ForeignKey(orm['profiles.user'], null=False)),
-            ('permission', models.ForeignKey(orm['auth.permission'], null=False))
+            ('user', models.ForeignKey(orm[u'profiles.user'], null=False)),
+            ('permission', models.ForeignKey(orm[u'auth.permission'], null=False))
         ))
         db.create_unique(m2m_table_name, ['user_id', 'permission_id'])
 
 
     def backwards(self, orm):
         # Deleting model 'User'
-        db.delete_table('profiles_user')
+        db.delete_table(u'profiles_user')
 
         # Removing M2M table for field groups on 'User'
-        db.delete_table(db.shorten_name('profiles_user_groups'))
+        db.delete_table(db.shorten_name(u'profiles_user_groups'))
 
         # Removing M2M table for field user_permissions on 'User'
-        db.delete_table(db.shorten_name('profiles_user_user_permissions'))
+        db.delete_table(db.shorten_name(u'profiles_user_user_permissions'))
 
 
     models = {
-        'auth.group': {
+        u'auth.group': {
             'Meta': {'object_name': 'Group'},
-            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
             'name': ('django.db.models.fields.CharField', [], {'unique': 'True', 'max_length': '80'}),
-            'permissions': ('django.db.models.fields.related.ManyToManyField', [], {'symmetrical': 'False', 'to': "orm['auth.Permission']", 'blank': 'True'})
+            'permissions': ('django.db.models.fields.related.ManyToManyField', [], {'to': u"orm['auth.Permission']", 'symmetrical': 'False', 'blank': 'True'})
         },
-        'auth.permission': {
-            'Meta': {'object_name': 'Permission', 'ordering': "('content_type__app_label', 'content_type__model', 'codename')", 'unique_together': "(('content_type', 'codename'),)"},
+        u'auth.permission': {
+            'Meta': {'ordering': "(u'content_type__app_label', u'content_type__model', u'codename')", 'unique_together': "((u'content_type', u'codename'),)", 'object_name': 'Permission'},
             'codename': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
-            'content_type': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['contenttypes.ContentType']"}),
-            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'content_type': ('django.db.models.fields.related.ForeignKey', [], {'to': u"orm['contenttypes.ContentType']"}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
             'name': ('django.db.models.fields.CharField', [], {'max_length': '50'})
         },
-        'contenttypes.contenttype': {
-            'Meta': {'object_name': 'ContentType', 'db_table': "'django_content_type'", 'ordering': "('name',)", 'unique_together': "(('app_label', 'model'),)"},
+        u'contenttypes.contenttype': {
+            'Meta': {'ordering': "('name',)", 'unique_together': "(('app_label', 'model'),)", 'object_name': 'ContentType', 'db_table': "'django_content_type'"},
             'app_label': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
-            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
             'model': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
             'name': ('django.db.models.fields.CharField', [], {'max_length': '100'})
         },
-        'profiles.user': {
+        u'profiles.user': {
             'Meta': {'object_name': 'User'},
             'address': ('django.db.models.fields.TextField', [], {'blank': 'True'}),
             'bio': ('django.db.models.fields.TextField', [], {'blank': 'True'}),
+            'city': ('django.db.models.fields.CharField', [], {'max_length': '50', 'blank': 'True'}),
             'date_joined': ('django.db.models.fields.DateTimeField', [], {'default': 'datetime.datetime.now'}),
             'date_updated': ('django.db.models.fields.DateTimeField', [], {'auto_now': 'True', 'blank': 'True'}),
-            'day_of_week': ('django.db.models.fields.CharField', [], {'max_length': '1', 'db_index': 'True', 'blank': 'True'}),
+            'day_of_week': ('django.db.models.fields.IntegerField', [], {'db_index': 'True', 'max_length': '1', 'null': 'True', 'blank': 'True'}),
             'email': ('django.db.models.fields.EmailField', [], {'max_length': '75', 'blank': 'True'}),
             'facebook_username': ('django.db.models.fields.CharField', [], {'max_length': '50', 'blank': 'True'}),
+            'featured': ('django.db.models.fields.BooleanField', [], {'default': 'False'}),
             'first_name': ('django.db.models.fields.CharField', [], {'max_length': '30', 'blank': 'True'}),
             'github_username': ('django.db.models.fields.CharField', [], {'max_length': '50', 'blank': 'True'}),
             'google': ('django.db.models.fields.CharField', [], {'max_length': '50', 'blank': 'True'}),
-            'groups': ('django.db.models.fields.related.ManyToManyField', [], {'related_name': "'user_set'", 'symmetrical': 'False', 'to': "orm['auth.Group']", 'blank': 'True'}),
-            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'gravatar_url': ('django.db.models.fields.URLField', [], {'max_length': '200', 'blank': 'True'}),
+            'groups': ('django.db.models.fields.related.ManyToManyField', [], {'symmetrical': 'False', 'related_name': "u'user_set'", 'blank': 'True', 'to': u"orm['auth.Group']"}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
             'is_active': ('django.db.models.fields.BooleanField', [], {'default': 'True'}),
+            'is_gravatar_verified': ('django.db.models.fields.BooleanField', [], {'default': 'False'}),
             'is_staff': ('django.db.models.fields.BooleanField', [], {'default': 'False'}),
             'is_superuser': ('django.db.models.fields.BooleanField', [], {'default': 'False'}),
             'jitsi': ('django.db.models.fields.CharField', [], {'max_length': '50', 'blank': 'True'}),
@@ -112,8 +122,10 @@ class Migration(SchemaMigration):
             'phone': ('django.db.models.fields.CharField', [], {'max_length': '50', 'blank': 'True'}),
             'skype': ('django.db.models.fields.CharField', [], {'max_length': '50', 'blank': 'True'}),
             'start_time': ('django.db.models.fields.TimeField', [], {'null': 'True', 'blank': 'True'}),
+            'state': ('django.db.models.fields.CharField', [], {'max_length': '50', 'blank': 'True'}),
+            'timezone': ('django.db.models.fields.CharField', [], {'default': "'America/Puerto_Rico'", 'max_length': '255'}),
             'twitter_username': ('django.db.models.fields.CharField', [], {'max_length': '50', 'blank': 'True'}),
-            'user_permissions': ('django.db.models.fields.related.ManyToManyField', [], {'related_name': "'user_set'", 'symmetrical': 'False', 'to': "orm['auth.Permission']", 'blank': 'True'}),
+            'user_permissions': ('django.db.models.fields.related.ManyToManyField', [], {'symmetrical': 'False', 'related_name': "u'user_set'", 'blank': 'True', 'to': u"orm['auth.Permission']"}),
             'username': ('django.db.models.fields.CharField', [], {'unique': 'True', 'max_length': '30'}),
             'website_url': ('django.db.models.fields.URLField', [], {'max_length': '50', 'blank': 'True'})
         }
