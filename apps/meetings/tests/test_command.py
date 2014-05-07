386cc557aaf3189a5ba0dd7ba7953aa7d9295785
Giovanni Collazo <hello@gcollazo.com>
2014-05-07 16:05:55 -0400

    Adds command test

---
+++ apps/meetings/tests/test_command.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+from datetime import timedelta
+from django.utils.timezone import now
+
+from apps.meetings.tests import MeetingBaseTestCase
+from apps.meetings.models import Meeting
+
+
+class UpdateMeetingsCommandTestCase(MeetingBaseTestCase):
+    def test_should_flag_available_in_past_as_unused(self):
+        m = Meeting.objects.create(state='available',
+            mentor=self.dude, datetime=now() - timedelta(hours=1, seconds=1))
+
+        self.assertEquals(Meeting.objects.filter(state='available').count(), 2)
+
+        self.call_command('update_meetings')
+
+        self.assertEquals(Meeting.objects.get(pk=m.id).state, 'unused')
+        self.assertEquals(Meeting.objects.filter(state='available').count(), 2)
+        self.assertEquals(Meeting.objects.filter(state='unused').count(), 1)
+
+    def test_should_create_missing_meetings(self):
+        self.assertEquals(Meeting.objects.filter(state='available').count(), 1)
+        self.call_command('update_meetings')
+        self.assertEquals(Meeting.objects.filter(state='available').count(), 2)
