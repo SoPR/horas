e67d849eef6289680c04cfef1f8fc1724c9d82da
Giovanni Collazo <hello@gcollazo.com>
2014-05-07 14:51:59 -0400

    Extract BaseTestCase

---
+++ apps/core/tests/__init__.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+from django.test import TestCase
+from django.core.management import call_command
+
+from apps.profiles.models import User
+
+
+class BaseTestCase(TestCase):
+    fixtures = ['apps/profiles/fixtures/dude.json']
+
+    def setUp(self):
+        self.dude = User.objects.get(username='dude')
+        self.call_command = call_command
+        self.call_command('create_notice_types')
+
+    def _login_user(self):
+        credentials = {'login': 'dude', 'password': 'thedude'}
+        return self.client.post('/accounts/login/', credentials)
